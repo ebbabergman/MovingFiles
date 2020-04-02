6248265babcbdd719d30847ede17e5e3e71ab5f4
ebbaberg <ebba.bergman@farmbio.uu.se>
2020-04-02 11:26:29 +0200

    Moves filesn done

--- Move_Prepped_ BBBC012v1.py
+++ Move_Prepped_ BBBC012v1.py
@@ -3,22 +3,24 @@
 
 import csv
 import os
+import shutil
 
 DIR = '/Users/ebbbe288/Documents/TestData/'
 FILE_NAME = '/Users/ebbbe288/Documents/TestData_bbc021v/bbbc021v1_labels.csv'
-# with open(FILE_NAME, newline='') as csvfile:
-#     data = list(csv.reader(csvfile))
-# print(data)
+IMAGE_DIR= '/Users/ebbbe288/Documents/TestData_bbc021v/images_bbbc021'
+IMAGE_NAME ='/bbbc021v1_%s.png' #Where %s is the image number
 
 ##Assumes row structure is ['image_number', 'compound', 'concentration', 'moa', 'plate', 'well', 'replicate']
 def sort_into_class_folders(row):
     if(str(row[3]) == 'moa') :     #Ignore header
         return
+    current_path = IMAGE_DIR + IMAGE_NAME  % str(row[0])
+    target_path = DIR + str(row[3])+"/" +str(row[0]) + ".png"
     dir_path = DIR + str(row[3])
     if not os.path.exists(dir_path):
         os.makedirs(dir_path)
         print(str(row))
-
+    shutil.copyfile(current_path, target_path)
 
 with open(FILE_NAME, 'r') as read_obj:
     # pass the file object to reader() to get the reader object
