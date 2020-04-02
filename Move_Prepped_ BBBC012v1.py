33d068eb316214a932f3d7d369ab124ccd38a116
ebbaberg <ebba.bergman@farmbio.uu.se>
2020-04-02 10:57:14 +0200

    new function

--- Move_Prepped_ BBBC012v1.py
+++ Move_Prepped_ BBBC012v1.py
@@ -2,6 +2,7 @@
 ## Data set first taken from bbbc021
 
 import csv
+import os
 
 
 FILE_NAME = '/Users/ebbbe288/Documents/TestData_bbc021v/bbbc021v1_labels.csv'
@@ -9,11 +10,15 @@ FILE_NAME = '/Users/ebbbe288/Documents/TestData_bbc021v/bbbc021v1_labels.csv'
 #     data = list(csv.reader(csvfile))
 # print(data)
 
+##Assumes row structure is ['image_number', 'compound', 'concentration', 'moa', 'plate', 'well', 'replicate']
+def sort_into_class_folders(row):
+     print(str(row))
+
 
 with open(FILE_NAME, 'r') as read_obj:
     # pass the file object to reader() to get the reader object
     csv_reader = csv.reader(read_obj, delimiter=";")
     # Iterate over each row in the csv using reader object
     for row in csv_reader:
-        # row variable is a list that represents a row in csv
-        print(row[0])
+        sort_into_class_folders(row)
+
