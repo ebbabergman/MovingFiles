685b8de2dcaf6ce6a8b3bba1d4d75bf537b9fde9
Ebba Bergman <Ebba.Bergman@farmbio.uu.se>
2022-02-03 15:10:55 +0000

    made all compounds 3-folds

--- Make_k_folds_stratified_SPECS.py
+++ Make_k_folds_stratified_SPECS.py
@@ -10,8 +10,8 @@ class MakeKFolds:
    
     def __init__(self,
                 labels_path = '/home/jovyan/Data/Specs/Specs_Labels.csv',
-                output_dir = '/home/jovyan/Inputs/SPECS_Top3_K_folds/',
-                include_groups = ["heat shock response signalling agonist", "phosphodiesterase inhibitor", "methyltransferase inhibitor"], #Empty for everything included,
+                output_dir = '/home/jovyan/Inputs/SPECS_K_folds/',
+                include_groups = [], #Empty for everything included,
                 include_header = "selected_mechanism",
                 exclude_groups = [], #Empty for everything included,
                 exclude_header = 'selected_mechanism',
