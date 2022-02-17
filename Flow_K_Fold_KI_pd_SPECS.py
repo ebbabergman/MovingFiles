4fa41b8956ec3fc391fe9719886ba21c9a874e15
Ebba Bergman <Ebba.Bergman@farmbio.uu.se>
2022-02-17 14:44:23 +0000

    , instead of ;

--- Flow_K_Fold_KI_pd_SPECS.py
+++ Flow_K_Fold_KI_pd_SPECS.py
@@ -221,7 +221,7 @@ class LeaveOneOut:
         if(self.exclude_images_path == ""):
             return df_used
         
-        df_bad_images = pd.read_csv(self.exclude_images_path , delimiter= ";")
+        df_bad_images = pd.read_csv(self.exclude_images_path , delimiter= ",")
         df_bad_images.columns= df_bad_images.columns.str.lower()
         df_do_not_use = pd.merge(df_used,df_bad_images, on = self.meta_data_header, how = "left" )
         df_do_not_use = df_do_not_use[df_do_not_use["total"] == 1 ] ## total ==1 means at least one flag has been raised and the image should be excluded
