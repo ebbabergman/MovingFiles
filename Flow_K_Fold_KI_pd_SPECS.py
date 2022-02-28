00bf39afae9d44c51c17c99e5d0c91808d7ec529
Ebba Bergman <Ebba.Bergman@farmbio.uu.se>
2022-02-28 14:28:08 +0000

    updates updated

--- Flow_K_Fold_KI_pd_SPECS.py
+++ Flow_K_Fold_KI_pd_SPECS.py
@@ -35,10 +35,6 @@ class LeaveOneOut:
                 name_to_leave_out = "" ,
                 k_fold = "1",
                 output_size = 1, # Percentage of original total size that should be used,
-                # Below for debugging
-               
-        
-                
                 ):
         self.labels_path = labels_path
         self.exclude_images_path = exclude_images_path
@@ -65,14 +61,15 @@ class LeaveOneOut:
     def update_settings(self,
                 labels_path = '/home/jovyan/Data/Specs/Specs_Labels.csv',
                 output_dir = '/home/jovyan/Inputs/Test/',
-                include_groups = ["heat shock response signalling agonist", "phosphodiesterase inhibitor", "methyltransferase inhibitor"], #Empty for everything included,
+                include_groups = ["[dmso]","DNA polymerase inhibitor", "NFkB pathway inhibitor","mTOR inhibitor", "topoisomerase inhibitor"],
                 include_header = "selected_mechanism",
+                class_column_header = "selected_mechanism",
+                exclude_images_path = "~/Outputs/CellProfiler/QC_Specs/QC_Specs_OnlyFlaggedAut_AllPlates.csv",
+                save_labels_dir = "./Outputs/",
+                #k_fold_dir = '/home/jovyan/Inputs/SPECS_K_folds/',
+                k_fold_dir = '/home/jovyan/Inputs/SPECS_QC_Automatic_Jordi_Controll_top4_K_folds/',
                 exclude_groups = [], #Empty for everything included,
                 exclude_header = 'selected_mechanism',
-                class_column_header = "selected_mechanism",
-                exclude_images_path = "",
-                save_labels_dir = '/home/jovyan/Inputs/Test/',
-                k_fold_dir = '/home/jovyan/Inputs/SPECS_Top3_K_folds/',
                 k_fold_name = "k_fold_%s.csv",#where /%s is the k_fold number
                 image_dir= '/home/jovyan/scratch-shared/Specs/MiPhHo/',
                 image_name ='%s.png', #Where %s is the image number,
@@ -83,7 +80,7 @@ class LeaveOneOut:
                 image_number_heading = "ImageNr",
                 name_to_leave_out = "" ,
                 k_fold = "1",
-                output_size = 1 # Percentage of original total size that should be used,
+                output_size = 1, # Percentage of original total size that should be used,            
                 ):
         self.labels_path = labels_path
         self.exclude_images_path = exclude_images_path
