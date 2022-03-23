56a22774352afcc5e2227fc0b7d598f4b7e3dcfe
Ebba Bergman <Ebba.Bergman@farmbio.uu.se>
2022-03-23 12:41:02 +0000

    trying to implement

--- GroupRows.py
+++ GroupRows.py
@@ -5,12 +5,12 @@ import numpy as np
 class GroupRows:
 
     staticmethod
-    def group_rows(data, keep_complete_index, number_of_groups):
+    def group_rows(data, keep_complete_header, number_of_groups):
         groups = [None]*number_of_groups
 
-        np_unique, np_index_unique = data[keep_complete_index].unique(unique_inverse = True)
+        np_unique, np_index_unique, counts = np.unique(data[keep_complete_header], return_inverse = True, return_counts = True)
 
-        min_per_fold = np_unique//number_of_groups
+        min_per_fold = len(np_unique)//number_of_groups
 
         for i in number_of_groups:
             in_group = np_unique(size =(1,min_per_fold) ,replace = False)
