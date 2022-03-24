c4e6b2a512fd815b03c5e45772aa22a8056a44ae
Ebba Bergman <Ebba.Bergman@farmbio.uu.se>
2022-03-24 13:06:37 +0000

    cleanup

--- GroupRows.py
+++ GroupRows.py
@@ -8,7 +8,7 @@ class GroupRows:
     def group_rows(data, keep_complete_header, number_of_groups):
         groups = [None]*number_of_groups
 
-        np_unique, np_index_unique, counts = np.unique(data[keep_complete_header], return_inverse = True, return_counts = True)
+        np_unique = np.unique(data[keep_complete_header], return_inverse = True, return_counts = True)
 
         min_per_fold = len(np_unique)//number_of_groups
 
@@ -17,7 +17,4 @@ class GroupRows:
             groups[i] = in_group
             np_unique = np.setdiff1d(np_unique,in_group)
         
-        #Should I return remaining np_unique or just divide them now?
-
-
-        return groups, np_unique # return "left over" groups?
+        return groups, np_unique
