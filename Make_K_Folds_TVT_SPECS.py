671329b14915933d68dbcb55877c699fbc113d31
Ebba Bergman <Ebba.Bergman@farmbio.uu.se>
2022-07-18 08:32:15 +0000

    start customizing, added todo's

--- Make_K_Folds_TVT_SPECS.py
+++ Make_K_Folds_TVT_SPECS.py
@@ -2,6 +2,7 @@ import csv
 import os
 from pickle import FALSE
 import shutil
+from xxlimited import Str
 import numpy as np
 import random
 import pandas as pd
@@ -22,17 +23,15 @@ class MakeKFolds:
                 include_header = "selected_mechanism",
                 class_column_header = "selected_mechanism",
                 exclude_groups = ["poscon","empty"],
-                exclude_header = "pert_type",
-                # Exclude images isn't used. Change? There has to be a simpler way to do this.
+                exclude_groups_header = "pert_type",
                 exclude_images_path = "/home/jovyan/Data/Specs/Flaggs/Cell_Profiler_Flagged_images_outside_nuclei_cut_82_149.csv",
                 intact_group_header = 'compound_id',
-                intact_control_group_headers = ['plate', 'well'], # NOTE: hard coded for 2 headers to to troubles with dataframe
-                meta_data_header = ['plate', 'well', 'site'],
+                unique_sample_headers = ['plate', 'well', 'site'],
                 image_number_heading = "nr",   
-                has_controls = False,
-                frac_of_controls_to_use = 1,
                 k_folds = "3",
                 divide_on_header = 'compound_id',
+                make_train_valid = True,
+                valid_fraction = 0.25 # 1 = 100%,  Percentage of images remaining afte the test set has been excluded
                 ):
         self.labels_path = labels_path
         self.output_dir = output_dir
@@ -40,33 +39,33 @@ class MakeKFolds:
         self.included_groups = include_groups
         self.include_header = include_header
         self.exclude_groups = exclude_groups
-        self.exclude_header = exclude_header
-        self.meta_data_header = meta_data_header
+        self.exclude_groups_header = exclude_groups_header
+        self.unique_sample_headers = unique_sample_headers
         self.image_number_heading = image_number_heading
         self.class_column_header =  class_column_header 
         self.intact_group_header = intact_group_header
-        self.intact_control_group_headers = intact_control_group_headers
-        self.has_controls = has_controls
-        self.frac_of_controls_to_use = frac_of_controls_to_use
-        self.frac_of_controls_to_use = frac_of_controls_to_use
         self.k_folds = int(k_folds)
         self.divide_on_header = divide_on_header
-
+        self.make_train_valid = make_train_valid
+        self.valid_fraction = valid_fraction
 
     def main(self):
-        print("Started get info.")
+        print("Started make k-folds.")
 
         df_base = pd.read_csv(self.labels_path , delimiter= ",")
         df_base.dropna(subset = [self.class_column_header], inplace=True)
         df_base.drop_duplicates(inplace=True)
 
         all_groups_that_could_be_included = self.get_included_groups(df_base)
-        df = df_base[df_base[self.include_header].isin(all_groups_that_could_be_included) & ~df_base[self.exclude_header].isin(self.exclude_groups)]
+        df = df_base[df_base[self.include_header].isin(all_groups_that_could_be_included) & ~df_base[self.exclude_groups_header].isin(self.exclude_groups)]
         self.included_groups = self.get_included_groups(df)
+
+        print("Included and exlcuded groups")
 
         k_folds_test = self.get_k_folds_test(df)
 
-        k_fold_train, k_fold_validation
+        if self.make_train_valid:
+            k_fold_train, k_fold_validation=  self.get_k_folds_tv(df, k_fold_test, self.valid_fraction)
 
         ##Make some statistics 
         s_statistics = df.groupby(self.class_column_header)[self.divide_on_header].nunique()
@@ -109,19 +108,19 @@ class MakeKFolds:
         k_fold_frac = 1/number_of_folds
         k_folds = [None]*number_of_folds
         group_n = {}
-        df_used_wells = pd.DataFrame()
         df_unused = df.copy()
 
         for group in self.included_groups:
             df_group = df[df[self.include_header].isin([group])]
-            if self.has_controls and group == 'negcon':
-                unique_controls = len(df_group[self.intact_control_group_headers].groupby(self.intact_control_group_headers))
-                group_n[group] = math.floor(unique_controls*k_fold_frac)
-            else:
-                group_n[group] = math.floor(df_group[self.divide_on_header].nunique()*k_fold_frac)
-            if group_n[group] < 1: group_n[group] = 1
+            # TODO START HERE
+            ## Is there an error here?
+            group_n[group] = math.floor(df_group[self.divide_on_header].nunique()*k_fold_frac)
+            if group_n[group] < 1: 
+                group_n[group] = 1
+                print("A group did not have enough unique groupings to have 1 unique entry per fold. Group:" + str(group) )
 
-        for k_fold in range(0,number_of_folds-1):
+
+        for k_fold in range(1,number_of_folds +1):
             df_fold= pd.DataFrame()
             for group in self.included_groups:
                 df_group = df_unused[df_unused[self.include_header].isin([group])]
@@ -130,42 +129,27 @@ class MakeKFolds:
                     df_unused.append(df_used)
                     df_group = df_unused[df_unused[self.include_header].isin([group])]
                     print("Every unit from group had been used, re-using values for group " + str(group) + ".")
-                
-                if self.has_controls and group == 'negcon':
-                    df_sampled, df_used_wells = self.getControlSampel( df_group, df_used_wells, group_n[group])
-                    df_fold = df_fold.append(df_sampled)
-                else:
-                    unique_entries = df_group[self.intact_group_header].unique()
-                    group_choice = np.random.choice(unique_entries, size = group_n[group])
-                    df_group_coice = df_group[df_group[self.divide_on_header].isin(group_choice)]
-                    df_fold = df_fold.append(df_group_coice)
-    #                 pd.merge(df1, df2, on=['A','B'], how='outer', indicator=True).query("_merge != 'both'")
-    #    .drop('_merge', axis=1)
-    #    .reset_index(drop=True)
-            df_unused = pd.concat([df_unused, df_fold, df_fold]).drop_duplicates(keep=False)
-            #df_unused =  df_unused[~df_unused.isin(df_fold)].dropna(how = 'all')
-            k_folds[k_fold] = df_fold
-        k_folds[number_of_folds-1] = df_unused
 
-        # if self.has_controls and df[df[self.class_column_header] == '[dmso]'].empty:
-        #     df_group = df[df[self.include_header].isin(['[dmso]'])]
-        #     df_sampled, df_used_wells, df = self.getControlSampel( df_group, df_used_wells, df, group_n['[dmso]'])
-        #     k_folds[number_of_folds-1] =  k_folds[number_of_folds-1].append(df_sampled)
-            
+                unique_entries = df_group[self.intact_group_header].unique()
+                group_choice = np.random.choice(unique_entries, size = group_n[group])
+                df_group_coice = df_group[df_group[self.divide_on_header].isin(group_choice)]
+                df_fold = df_fold.append(df_group_coice)
+            df_unused = pd.concat([df_unused, df_fold, df_fold]).drop_duplicates(keep=False)
+            k_folds[k_fold] = df_fold
+
+       # TODO use  df_unused but put them into the other fold iterativel ( I feel like I already did this somewhere..)
+
+        print("Made test sets for k-folds")    
         return k_folds
 
-    def getControlSampel(self, df_group, df_used_wells, n_sample):
-        if df_group.empty:
-            df_group = df_used_wells
+    def get_k_folds_tv(self, df, k_fold_test):
+        number_of_folds = self.k_folds
+        k_fold_train = [None]*number_of_folds
+        k_fold_validation = [None]*number_of_folds
 
-        df_control = pd.DataFrame({'count' : df_group[self.intact_control_group_headers].groupby(self.intact_control_group_headers).size()}).reset_index()
-        sampled_well_index = np.random.choice(df_control.index, n_sample)
-        sampled_wells = df_control.iloc[sampled_well_index]
-        df_sampled= df_group.merge(sampled_wells)
-        df_sampled.drop('count', axis = 1, inplace = True)
+        # TODO implement the actual function
+        return k_fold_train, k_fold_validation
 
-        df_used_wells = df_used_wells.append(df_sampled)
-        return df_sampled, df_used_wells, 
 
 if __name__ == "__main__":
     MakeKFolds().main()
