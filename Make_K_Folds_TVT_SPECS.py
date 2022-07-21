c76c829faebe5c2e05b429550dcfa2ba6d1122e3
Ebba Bergman <Ebba.Bergman@farmbio.uu.se>
2022-07-21 08:24:33 +0000

    top 10 classes for smaller run

--- Make_K_Folds_TVT_SPECS.py
+++ Make_K_Folds_TVT_SPECS.py
@@ -16,10 +16,9 @@ class MakeKFolds:
                    
     def __init__(self,
                 labels_path = '/home/jovyan/Data/Specs/Labels.csv',
-                output_dir='/home/jovyan/Inputs/SPECS_Nuclei_Cutoff_CP_AUT_K_folds/',
-                include_groups = [], #Empty for everything included,
-                #include_groups = ["negcon","heat shock response signalling agonist", "phosphodiesterase inhibitor", "methyltransferase inhibitor","DILI","HDAC inhibitor","topoisomerase inhibitor", "mTOR inhibitor","NFkB pathway inhibitor","JAK inhibitor","pregnane x receptor agonist"], #Empty for everything included,
-                #include_groups = ["negcon","DNA polymerase inhibitor", "mTOR inhibitor", "topoisomerase inhibitor"],
+                output_dir='/home/jovyan/Inputs/SPECS_Nuclei_Cutoff_CP_AUT_K_folds_top10/',
+               # include_groups = [], #Empty for everything included,
+                include_groups = ["negcon","heat shock response signalling agonist", "phosphodiesterase inhibitor", "methyltransferase inhibitor","DILI","HDAC inhibitor","topoisomerase inhibitor", "mTOR inhibitor","NFkB pathway inhibitor","JAK inhibitor","pregnane x receptor agonist"]                    #include_groups = ["negcon","DNA polymerase inhibitor", "mTOR inhibitor", "topoisomerase inhibitor"],
                 include_header = "selected_mechanism",
                 class_column_header = "selected_mechanism",
                 exclude_groups = ["poscon","empty"],
