c28452dd52a0bb68475213006ab2d8a6925d996b
Ebba Bergman <Ebba.Bergman@farmbio.uu.se>
2022-09-23 12:10:37 +0000

    make a class to inherit

---
+++ Make_K_Folds_TVT.py
@@ -0,0 +1,11 @@
+import csv
+import os
+from pickle import FALSE
+import shutil
+from xxlimited import Str
+import numpy as np
+import random
+import pandas as pd
+from pandas.core import indexing
+import math
+
