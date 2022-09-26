a64e5afee17f537bf0913e5fd94a967c3e2c197a
Ebba Bergman <Ebba.Bergman@farmbio.uu.se>
2022-09-26 14:01:11 +0000

    top 10 moa

--- Make_K_Folds_TVT_SPECS.py
+++ Make_K_Folds_TVT_SPECS.py
@@ -18,13 +18,15 @@ class MakeKFolds:
     def __init__(self,
                  #labels_path="/home/jovyan/Data/Specs/Specs_Labels_First_MiPhHo.csv", #"smaller images"
                  labels_path="/home/jovyan/Data/Specs/Labels.csv",#bigger images
-                 output_dir='/home/jovyan/Inputs/SPECS_16Bit_JordisTop10_moa_PhilAnkit_no_negcon_5_folds/',
+                 output_dir='/home/jovyan/Inputs/SPECS_16Bit_Top10_MoA_no_negcon_5_folds/',
                  #
                  #include_groups = [], #Empty for everything included,
                  #Jordis top 10
-                 include_groups = ["Aurora kinase inhibitor","tubulin polymerization inhibitor","JAK inhibitor","protein synthesis inhibitor","HDAC inhibitor","topoisomerase inhibitor","PARP inhibitor","ATPase inhibitor","retinoid receptor agonist","HSP inhibitor"],
+                 #include_groups = ["Aurora kinase inhibitor","tubulin polymerization inhibitor","JAK inhibitor","protein synthesis inhibitor","HDAC inhibitor","topoisomerase inhibitor","PARP inhibitor","ATPase inhibitor","retinoid receptor agonist","HSP inhibitor"],
                  # top10 good images (nuclei cut off)
                  #include_groups=["heat shock response signalling agonist","phosphodiesterase inhibitor","DILI","methyltransferase inhibitor","estrogen receptor alpha modulator","cyclooxygenase inhibitor","pregnane x receptor agonist","PPAR receptor agonist","protein synthesis inhibitor","CC chemokine receptor antagonist"],
+                 # TOP 10 MoA
+                 include_groups = ["cyclooxygenase inhibitor", "phosphodiesterase inhibitor", "EGFR inhibitor", "HDAC inhibitor", "topoisomerase inhibitor", "glucocorticoid receptor agonist", "CC chemokine receptor antagonist", "adenosine receptor antagonist",  "histone lysine methyltransferase inhibitor", "HSP inhibitor"],  
                  # include_groups = ["negcon", "heat shock response signalling agonist", "DILI", "estrogen receptor alpha modulator", "phosphodiesterase inhibitor",  "cyclooxygenase inhibitor"], #Empty for everything included,
                  # include_groups = ["negcon","heat shock response signalling agonist", "phosphodiesterase inhibitor", "methyltransferase inhibitor","DILI","HDAC inhibitor","topoisomerase inhibitor", "mTOR inhibitor","NFkB pathway inhibitor","JAK inhibitor","pregnane x receptor agonist"],                    #include_groups = ["negcon","DNA polymerase inhibitor", "mTOR inhibitor", "topoisomerase inhibitor"],
                  #include_groups = ['pregnane x receptor agonist', 'DILI', 'estrogen receptor alpha modulator', 'tubulin polymerization inhibitor', 'topoisomerase inhibitor', 'heat shock response signalling agonist', 'methyltransferase inhibitor', 'aryl hydrocarbon receptor agonist', 'estrogen receptor alpha agonist', 'mitochondrial toxicity  agonist', 'retinoid receptor agonist', 'protein synthesis inhibitor', 'phosphodiesterase inhibitor', 'DNA polymerase inhibitor', 'mTOR inhibitor', 'PPAR receptor agonist', 'glucocorticoid receptor agonist', 'ATPase inhibitor', 'cyclooxygenase inhibitor', 'NFkB pathway inhibitor', 'angiotensin converting enzyme inhibitor', 'adenosine receptor antagonist', 'PARP inhibitor', 'JAK inhibitor', 'HSP inhibitor', 'HDAC inhibitor',  'CC chemokine receptor antagonist', 'Aurora kinase inhibitor','negcon'],
@@ -32,8 +34,8 @@ class MakeKFolds:
                  class_column_header="moa",
                  excluded_groups=[["negcon","poscon", "empty"], ["P015085"]],
                  excluded_groups_headers=["pert_type", "plate"],
-                 #exclude_images_path="/home/jovyan/Data/Specs/Flaggs/16Bit_images_nuclei_cut_above149.csv",
-                 exclude_images_path="",
+                 exclude_images_path="/home/jovyan/Data/Specs/Flaggs/16Bit_images_nuclei_cut_above149.csv",
+                 #exclude_images_path="",
                  intact_group_header='compound_id',
                  unique_sample_headers=['plate', 'well', 'site'],
                  image_number_heading="ImageNr",
