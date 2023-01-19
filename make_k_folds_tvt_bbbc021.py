e3440296dcc03cba52fe4d78f52ed79eced07e8a
Ebba Bergman <Ebba.Bergman@farmbio.uu.se>
2023-01-19 13:50:06 +0000

    automatic formatting

--- make_k_folds_tvt_bbbc021.py
+++ make_k_folds_tvt_bbbc021.py
@@ -5,49 +5,52 @@ from make_k_folds_tvt import MakeTVTSets
 
 
 class MakeKFoldsTVTBBBC021(MakeTVTSets):
-                   
+
     def __init__(self,
-                labels_path = "/home/jovyan/Data/BBBC021/BBBC021_Labels.csv",
-                output_dir='/home/jovyan/Inputs/TEST_BBBC021_leave_one_out/',
-                include_groups = [], #Empty for everything included,
-                include_header = "moa",
-                class_column_header = "moa",
-                excluded_groups = [["DMSO","Cholesterol-lowering","Eg5 inhibitors"]],
-                #excluded_groups = [["DMSO"]],
-                excluded_groups_headers = ["moa"],
-                exclude_images_path = "",
-                intact_group_header = 'compound',
-                unique_sample_headers = ["ImageNumber"],
-                image_number_heading = "ImageNumber",   
-                k_folds = "3",
-                divide_on_header = 'compound',
-                make_train_valid = True,
-                valid_fraction = 0.25, # 1 = 100%,  Percentage of images remaining afte the test set has been excluded
-                leave_one_out = True,
-                make_unique_validation = True,
-                non_unique_divider = ['concentration', 'moa', 'compound', 'Replicate']
-                ):
-        super().__init__(labels_path = labels_path,
-                output_dir =output_dir,
-                include_groups = include_groups,
-                include_header = include_header,
-                class_column_header = class_column_header,
-                excluded_groups = excluded_groups,
-                excluded_groups_headers = excluded_groups_headers,
-                exclude_images_path = exclude_images_path,
-                intact_group_header = intact_group_header,
-                unique_sample_headers = unique_sample_headers,
-                image_number_heading = image_number_heading,   
-                k_folds = k_folds,
-                divide_on_header = divide_on_header,
-                valid_fraction = valid_fraction,
-                non_unique_divider = non_unique_divider)
+                 labels_path="/home/jovyan/Data/BBBC021/BBBC021_Labels.csv",
+                 output_dir='/home/jovyan/Inputs/TEST_BBBC021_leave_one_out/',
+                 include_groups=[],  # Empty for everything included,
+                 include_header="moa",
+                 class_column_header="moa",
+                 excluded_groups=[
+                     ["DMSO", "Cholesterol-lowering", "Eg5 inhibitors"]],
+                 #excluded_groups = [["DMSO"]],
+                 excluded_groups_headers=["moa"],
+                 exclude_images_path="",
+                 intact_group_header='compound',
+                 unique_sample_headers=["ImageNumber"],
+                 image_number_heading="ImageNumber",
+                 k_folds="3",
+                 divide_on_header='compound',
+                 make_train_valid=True,
+                 # 1 = 100%,  Percentage of images remaining afte the test set has been excluded
+                 valid_fraction=0.25,
+                 leave_one_out=True,
+                 make_unique_validation=True,
+                 non_unique_divider=['concentration',
+                                     'moa', 'compound', 'Replicate']
+                 ):
+        super().__init__(labels_path=labels_path,
+                         output_dir=output_dir,
+                         include_groups=include_groups,
+                         include_header=include_header,
+                         class_column_header=class_column_header,
+                         excluded_groups=excluded_groups,
+                         excluded_groups_headers=excluded_groups_headers,
+                         exclude_images_path=exclude_images_path,
+                         intact_group_header=intact_group_header,
+                         unique_sample_headers=unique_sample_headers,
+                         image_number_heading=image_number_heading,
+                         k_folds=k_folds,
+                         divide_on_header=divide_on_header,
+                         valid_fraction=valid_fraction,
+                         non_unique_divider=non_unique_divider)
         self.leave_one_out = leave_one_out
         self.make_unique_validation = make_unique_validation
 
     def main(self):
         print("Started making divisions for runs for BBBC021.")
-        if self.leave_one_out :
+        if self.leave_one_out:
             if self.make_unique_validation:
                 self.make_leave_one_out()
             else:
@@ -59,6 +62,7 @@ class MakeKFoldsTVTBBBC021(MakeTVTSets):
                 self.make_k_folds_train_test()
 
         print("Finished. Find output in: " + self.output_dir)
+
 
 if __name__ == "__main__":
     MakeKFoldsTVTBBBC021().main()
